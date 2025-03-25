ee2a51406dbf65317a3ee6667b5a2449c571ff19
Danila <danil.lysunckin@gmail.com>
2025-03-25 14:26:23 +0500

    tokarev init

--- webapp/src/main/resources/static/scripts/main.js
+++ webapp/src/main/resources/static/scripts/main.js
@@ -1,5 +1,6 @@
 function deleteIssue(issueId) {
     let token = $("meta[name='_csrf']").attr("content");
+    alert(issueId)
     $.ajax({
         url: '/forum/issue/delete',
         headers: {'X-CSRF-TOKEN': token},
